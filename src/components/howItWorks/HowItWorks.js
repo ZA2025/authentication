2dcedf931277c880f1f438434d7bbcff5a436efc
ZA2025 <zainabal2025@gmail.com>
2026-03-25 14:05:01 +0000

    fix  case-sensitivity mismatch

--- src/components/howItWorks/HowItWorks.js
+++ src/components/howItWorks/HowItWorks.js
@@ -19,7 +19,7 @@ const HowItWorks = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dum.</p>
                     </div>
                     <div className={styles.HowItem}>
-                        <Image src="/icons/jar.svg" alt="How It Works" width={100} height={100} />
+                        <Image src="/icons/Jar.svg" alt="How It Works" width={100} height={100} />
                         <h2 className={styles.HowItemTitle}>Step 3 - Enjoy your Cookies</h2>
                         <p>Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
                     </div>
